9642923cf98e358d6c9a28295eb2612eeb0c365f
Lucas Crouch <lucascrouch72@gmail.com>
2022-08-28 19:49:29 +0800

    added specific age

--- baby_tracker.py
+++ baby_tracker.py
@@ -3,20 +3,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+import math
 
-#writing simple text
+#get data
 growth = pd.read_csv("csv/Kosmo Emery Crouch_growth.csv")
 formula = pd.read_csv("csv/Kosmo Emery Crouch_formula.csv")
 pumped = pd.read_csv("csv/Kosmo Emery Crouch_pumped.csv")
 
+#calculate age stuff
+datetime.strptime(growth.iloc[-1]["Time"], '%Y/%m/%d, %H:%M')
+age = datetime.now() - datetime.strptime(growth.iloc[-1]["Time"], '%Y/%m/%d, %H:%M')
+days = age.days
+hours = age.seconds / 60 // 60
+minutes = math.floor((age.seconds / 60 / 60 - hours) * 60)
+seconds = math.floor((((age.seconds / 60 / 60 - hours) * 60) - minutes) * 60)
 
+#description
 st.text("This is the growth and development data for Kosmo Emery Crouch")
+st.text(f"Kosmo is {days} days, {int(hours)} hours, {minutes} minutes, {seconds} seconds old")
 
 growth["kg"] = growth["Weight"].apply(lambda x: float(x[:-3]))
 growth["Time"] = growth["Time"].apply(lambda x: datetime.strptime(x[:-7], '%Y/%m/%d'))
 growth.drop(growth[growth["kg"] == 0].index, inplace = True)
 
-
+#weight gain chart
 fig, ax = plt.subplots()
 plt.style.use("fivethirtyeight")
 plt.figure(figsize=(20, 10))
@@ -26,7 +36,7 @@ plt.title("Sample Time Series Plot")
 ax.plot(growth["Time"], growth["kg"])
 st.pyplot(fig)
 
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+# Pie chart with ratio of milk to formula
 labels = "Formula", "Moma's Milk"
 sizes = formula["Amount (ml)"].sum(
 ), pumped["Amount (ml)"].sum()
